5283bdaad59f39dd002eac1127bec83e85eab90a
Phan Thị Quỳnh Như <82049190+NHU0606@users.noreply.github.com>
2023-05-25 00:38:47 +0700

    bird fly with 4 direction

--- assets/Script/BirdController.ts
+++ assets/Script/BirdController.ts
@@ -1,49 +1,70 @@
-import { _decorator, Component, Node, Vec3 } from 'cc';
+import { _decorator, ColliderComponent, Component, Node, Vec3, Quat } from 'cc';
 const { ccclass, property } = _decorator;
 
 enum BirdDirection {
     Left,
     Right,
     TopLeft,
-    TopRight,
-    Die
+    TopRight
 }
 
 @ccclass('BirdController')
 export class BirdController extends Component {
     private birdNode: Node;
-    private birdSpeed: number = 100;
+    private birdSpeed: number = 170;
     private currentDirection: BirdDirection = BirdDirection.TopRight;
+    private directionChangeDelay: number = 1;
+    private directionChangeTimer: number = 0;
+
 
     protected onLoad(): void {
         this.birdNode = this.node;
+        this.directionChangeTimer = this.directionChangeDelay;
     }
 
-    protected update(dt: number): void {
-        const movement = new Vec3()
-
+    protected moveBird(dt: number): void {
+        const movement = new Vec3(0,0,0);
+       
         switch (this.currentDirection) {
             case BirdDirection.Left:
                 movement.x -= this.birdSpeed*dt;
+                this.birdNode.setRotationFromEuler(0, 180, 0);
                 break;
             case BirdDirection.Right:
                 movement.x += this.birdSpeed*dt;
+                this.birdNode.setRotationFromEuler(0, 0, 0);
                 break;
             case BirdDirection.TopLeft:
                 movement.x -= this.birdSpeed*dt;
                 movement.y += this.birdSpeed*dt; 
+                this.birdNode.setRotationFromEuler(0, 135, 0);
                 break;
             case BirdDirection.TopRight:
                 movement.x += this.birdSpeed*dt;
                 movement.y += this.birdSpeed*dt;
+                this.birdNode.setRotationFromEuler(0, 45, 0);
+                break;
         }
         this.birdNode.position = this.birdNode.position.add(movement);
     }
 
-    protected setDirection(direction: BirdDirection): void {
-        const directionCount = Object.keys(BirdController).length / 2;
-        const nextDirection = (this.currentDirection + 1) % directionCount; 
+    protected updateDirection(dt: number): void {
+        this.directionChangeTimer -= dt;
+        if (this.directionChangeTimer <= 0) {
+            this.setDirection();
+            this.directionChangeTimer = this.directionChangeDelay;
+        }
+    }
+
+    protected setDirection(): void {
+        const directionCount = Object.keys(BirdDirection).length / 2;
+        const nextDirection = (this.currentDirection + 1) % directionCount;
         this.currentDirection = nextDirection;
+    }
+    
+    protected update(dt: number): void {
+        this.moveBird(dt);
+        this.updateDirection(dt);
     }
 }
 
